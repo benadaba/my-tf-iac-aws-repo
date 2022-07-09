0328e4dc6cde8b03202377b510adb6cbb565c40e
Bernard Antwi Adabankah <adabankah.com@gmail.com>
2022-07-09 07:04:04 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "ubuntukey"
+  default     =  "ubuntukey-us-east-2"
 }
 
 variable "instance_type" {
